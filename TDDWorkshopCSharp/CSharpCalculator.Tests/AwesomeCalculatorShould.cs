664b1b6022e9ded59456a0c0943f3727fd5a4982
Andrei Dogariu <adogariu@codeless.com>
2023-11-15 15:05:27 +0400

    init

--- TDDWorkshopCSharp/CSharpCalculator.Tests/AwesomeCalculatorShould.cs
+++ TDDWorkshopCSharp/CSharpCalculator.Tests/AwesomeCalculatorShould.cs
@@ -1,4 +1,5 @@
 using FluentAssertions;
+using Xunit.Sdk;
 
 namespace CSharpCalculator.Tests
 {
@@ -16,6 +17,67 @@ namespace CSharpCalculator.Tests
 
             // Assert
             sut.Should().NotBeNull("we just created an instance.");
+        }
+
+
+        [Fact]
+        public void Return_0_If_Empty_String_Is_Passed()
+        {
+
+            // Arrange (sut = System Under Test)
+            Calculator sut;
+
+            // Act
+            sut = new Calculator();
+            var result = sut.Add(string.Empty);
+
+            // Assert
+            result.Should().Be(0);
+        }
+
+
+        [Fact]
+        public void Return_Integer_Value_If_String_Has_Single_Integer()
+        {
+
+            // Arrange (sut = System Under Test)
+            Calculator sut;
+
+            // Act
+            sut = new Calculator();
+            var result = sut.Add("1");
+
+            // Assert
+            result.Should().Be(1);
+        }
+
+
+        [Fact]
+        public void Return_Sum_Of_The_Passed_Values()
+        {
+
+            // Arrange (sut = System Under Test)
+            Calculator sut;
+
+            // Act
+            sut = new Calculator();
+            var result = sut.Add("1,2");
+
+            // Assert
+            result.Should().Be(3);
+        }
+
+        [Fact]
+        public void Throw_Unsupported_Arguments()
+        {
+
+            // Arrange (sut = System Under Test)
+            Calculator sut;
+
+            // Act
+            sut = new Calculator();
+            // Assert
+            Assert.Throws<UnSupportedArgumentsException>(() => sut.Add("abc"));
         }
     }
 }
--- TDDWorkshopCSharp/CSharpCalculator/Calculator.cs
+++ TDDWorkshopCSharp/CSharpCalculator/Calculator.cs
@@ -2,6 +2,23 @@
 {
     public class Calculator
     {
-
+        public int Add(string param)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(param))
+                {
+                    return 0;
+                }
+                else
+                {
+                    return param.Split(",").Select(a => int.Parse(a)).Sum();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new UnSupportedArgumentsException();
+            }
+        }
     }
 }
