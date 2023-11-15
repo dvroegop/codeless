e4ffd6b266a87d2eb6850b601f70b7b9827ebbc9
Madalina Georgiana Chelariu <mgeorgianachelariu@codeless.com>
2023-11-15 15:51:21 +0400

    Fix code

--- TDDWorkshopCSharp/CSharpCalculator.Tests/AwesomeCalculatorShould.cs
+++ TDDWorkshopCSharp/CSharpCalculator.Tests/AwesomeCalculatorShould.cs
@@ -24,7 +24,7 @@ namespace CSharpCalculator.Tests
 
             // Arrange (sut = System Under Test)
             Calculator sut;
-         
+
             // Act
             sut = new Calculator();
 
@@ -32,6 +32,24 @@ namespace CSharpCalculator.Tests
 
             // Assert
             value.Should().Be(0);
+        }
+
+        [Theory]
+        [InlineData("1", 1)]
+        [InlineData("1,2", 3)]
+        public void ReturnSumOfNumbers(string input, int output)
+        {
+
+            // Arrange (sut = System Under Test)
+            Calculator sut;
+
+            // Act
+            sut = new Calculator();
+
+            int value = sut.Add(input);
+
+            // Assert
+            value.Should().Be(output);
         }
 
         [Fact]
@@ -65,5 +83,7 @@ namespace CSharpCalculator.Tests
             // Assert
             act.Should().Throw<InvalidOperationException>().WithMessage($"{valueToAdd} is not a numeric value");
         }
+
+
     }
 }
