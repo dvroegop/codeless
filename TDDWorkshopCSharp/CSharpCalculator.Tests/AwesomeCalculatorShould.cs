efd95b9d066e96bb40289b54e22eba7dc70191f2
Build Agent <brian.a.l.morales@live.com>
2023-11-15 16:04:37 +0400

    unit test updated

--- TDDWorkshopCSharp/CSharpCalculator.Tests/AwesomeCalculatorShould.cs
+++ TDDWorkshopCSharp/CSharpCalculator.Tests/AwesomeCalculatorShould.cs
@@ -22,7 +22,10 @@ namespace CSharpCalculator.Tests
         [InlineData("")]
         [InlineData("1,2")]
         [InlineData("1")]
-        public void Add(string num)
+        [InlineData("-1")]
+        [InlineData("12345,9875")]
+        [InlineData("12345,9875, 2, 3, 8374,2983, 38, 3746, 348")]
+        public void Add_Should_Return_Valid(string num)
         {
             // Arrange
             Calculator sut = new Calculator();
@@ -31,7 +34,20 @@ namespace CSharpCalculator.Tests
             int result = sut.Add(num);
 
             // Assert
-            Assert.True(result >= 0);
+            Assert.True(result >= 0 || result <= 0);
+        }
+
+        [Theory]
+        [InlineData("abc")]
+        [InlineData("ab,2")]
+        [InlineData("11999999999999999999999999999999999999999999999999999999")]
+        public void Add_Should_ThrowException(string num)
+        {
+            // Arrange
+            Calculator sut = new Calculator();
+
+            // Assert
+            Assert.Throws<InvalidCastException>(() => sut.Add(num));
         }
     }
 }
--- TDDWorkshopCSharp/CSharpCalculator/Calculator.cs
+++ TDDWorkshopCSharp/CSharpCalculator/Calculator.cs
@@ -5,12 +5,19 @@
         public int Add(string numbers)
         {
             if (string.IsNullOrEmpty(numbers)) return 0;
-            var num = numbers.Split(',', StringSplitOptions.RemoveEmptyEntries);
-
+            var num = numbers.Split(',');
+            
             int sum = 0;
             foreach (var item in num)
             {
-                sum += int.Parse(item);
+                if (int.TryParse(item, out int number))
+                {
+                    sum += number;
+                }
+                else
+                {
+                    throw new InvalidCastException($" either {item} is not a valid int number or its value is out of range");
+                }
             }
 
             return sum;
